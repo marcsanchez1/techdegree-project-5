b261686e5e10c22b2490d06ca50d230773387aed
Marc Sanchez <marcsanchez@me.com>
2019-10-01 22:12:30 -0400

    Added search filter functionality

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 
+// Settings for lightbox
 $(document).ready(function($) {
       $('a[data-rel^=lightcase]').lightcase({
         transition: 'fadeInline',
-        speedIn: 500,
-        speedOut: 350,
         showSequenceInfo: false,
         showTitle: true,
       });
     });
+
+// Search to filter image based on user's input and image alt tag
+$('#photo-search').keyup(function() {
+  let val = $.trim(this.value);
+  // val = val.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  if (val === '') {
+    $('img').show();
+  } else {
+    $('img').hide();
+    $('img').each((i, img) => {
+      let imgAlt = img.alt.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+      if((imgAlt.toLowerCase()).indexOf(val.toLowerCase()) > -1) {
+        $(img).show();
+      }
+    });
+  }
+});
