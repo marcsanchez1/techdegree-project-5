8aebc8c7ea9f525a569c3feb821f992c068b1ea2
Marc Sanchez <marcsanchez@me.com>
2019-10-03 22:22:04 -0400

    Update JS for Search Filter

--- js/script.js
+++ js/script.js
@@ -6,24 +6,23 @@ $("#search-input").sticky({
 
 // Settings for lightbox
 $(document).ready(function($) {
-      $('a[data-rel^=lightcase]').lightcase({
-        transition: 'fadeInline',
-        showSequenceInfo: false,
-        showTitle: true,
-      });
-    });
+  $('a[data-rel^=lightcase]').lightcase({
+    transition: 'fadeInline',
+    showSequenceInfo: false,
+    showTitle: true,
+  });
+});
 
 // Search to filter image based on user's input and image alt tag
 $('#photo-search').keyup(function() {
   let val = $.trim(this.value);
-  // val = val.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
   if (val === '') {
     $('img').show();
   } else {
     $('img').hide();
     $('img').each((i, img) => {
-      let imgAlt = img.alt.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-      if((imgAlt.toLowerCase()).indexOf(val.toLowerCase()) > -1) {
+      let altText = img.alt.normalize('NFD');
+      if((altText.toLowerCase()).indexOf(val.toLowerCase()) > -1) {
         $(img).show();
       } 
     });
